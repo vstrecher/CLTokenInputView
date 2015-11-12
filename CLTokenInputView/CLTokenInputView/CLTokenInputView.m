7d5d3c65da4f9f6aaa50baab12f9acadda120c7d
Masha Patani <masha.patani@gmail.com>
2015-11-12 16:56:43 +0300

    Customized font for left label

--- CLTokenInputView/CLTokenInputView/CLTokenInputView.m
+++ CLTokenInputView/CLTokenInputView/CLTokenInputView.m
@@ -46,9 +46,6 @@ static CGFloat const FIELD_MARGIN_X = 4.0; // Note: Same as CLTokenView.PADDING_
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.delegate = self;
-    if (self.textFieldFont) {
-        self.textField.font = self.textFieldFont;
-    }
     self.additionalTextFieldYOffset = 0.0;
     if (![self.textField respondsToSelector:@selector(defaultTextAttributes)]) {
         self.additionalTextFieldYOffset = 1.5;
@@ -473,6 +470,24 @@ static CGFloat const FIELD_MARGIN_X = 4.0; // Note: Same as CLTokenView.PADDING_
 
 
 #pragma mark - (Optional Views)
+
+- (void)setTextFieldFont:(UIFont *)textFieldFont {
+    if (!textFieldFont) {
+        return;
+    }
+    
+    _textFieldFont = textFieldFont;
+    self.textField.font = _textFieldFont;
+}
+
+- (void)setLeftLabelFont:(UIFont *)leftLabelFont {
+    if (!leftLabelFont) {
+        return;
+    }
+    
+    _leftLabelFont = leftLabelFont;
+    self.fieldLabel.font = _leftLabelFont;
+}
 
 - (void)setFieldName:(NSString *)fieldName
 {
